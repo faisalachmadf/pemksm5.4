0483cdf9c14ebd0c2d7cb618ba95aa6df99a82ab
faisalachmadf <faisalachmadf@gmail.com>
2017-11-24 17:09:19 +0700

    penambahan css

--- resources/views/frame_depan/partindex/bannerindex.blade.php
+++ resources/views/frame_depan/partindex/bannerindex.blade.php
@@ -1,26 +1,34 @@
 <div class="banner">
+	
+	
      <div class="container">
 	       <div class="banner-inner">
 						<div class="callbacks_container">
 						<ul class="rslides callbacks callbacks1" id="slider4">
 							<li class="callbacks1_on" style="display: block; float: left; position: relative; opacity: 1; z-index: 2; transition: opacity 500ms ease-in-out;">
+								
 								<div class="banner-info">
 								<h3>WHAT IS LIKE TO WORK AS A SUPERMODEL ON WOMEN’S FASHION</h3>
 								<p>Lorem ipsum dolor sit amet</p>
 								</div>
+
 							</li>
 							<li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 500ms ease-in-out;">
+								
 								<div class="banner-info">
 								   <h3>FANTASTIC MAN MAGAZINE AND ITS INFLUENCE ON MEN’S FASHION</h3>
 								 <p>Lorem ipsum dolor sit amet</p>
 								</div>
+
 							</li>
 							<li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 
 							<!--/header navigation-->  500ms ease-in-out;">
+								
 								<div class="banner-info">
 								 <h3>BIRO PEMERINTAHAN DAN KERJA SAMA SEKRETARIAT DAERAH PROVINSI JAWA BARAT</h3>
 								<p>Lorem ipsum dolor sit amet</p>
 								</div>	
+
 							</li>
 						</ul>
 						</div>
